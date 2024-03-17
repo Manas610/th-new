44d71af4f84b053424012d0e4a75348146a3f1f6
Manas <singhmanas610@gmail.com>
2024-03-17 18:49:04 +0530

    fix 2

--- src/app/page.js
+++ src/app/page.js
@@ -16,15 +16,16 @@ export default function Home() {
           <div>
             <span className="bg-red-100 p-1 rounded text-red-700 font-medium my-4">About Tycoon Hamlet</span>
             <p className="my-4 font-extrabold text-black text-4xl">We Are Fully Integrated Digital Business</p>
-            <p className="text-black pr-8 my-4 mr-16 leading-6 font-sans text-justify">Our company specializes in creating stunning, user-friendly websites that are tailored to meet the unique needs 
+            <p className="text-black pr-8 my-4 mr-16 leading-6 font-sans text-justify">Our company specializes in creating stunning, 
+            user-friendly websites that are tailored to meet the unique needs 
             of our clients. We offer a wide range of web development services, including website design, e-commerce development, 
             and custom web application development. Our team of experienced developers and designers work closely with clients 
             to understand their goals and create a website that will help them achieve success online. We pride ourselves on 
-            delivering high-quality work and exceptional customer service. Whether you're a small startup or a large corporation, 
+            delivering high-quality work and exceptional customer service. Whether you&apos;re a small startup or a large corporation, 
             we have the skills and expertise to help take your online presence to the next level.</p>
             <div className="mt-8">
                 <Button outline gradientDuoTone="greenToBlue">
-                    Get Started
+                    Get Started <span className='font-extrabold pl-2'>&#x2192;</span>
                 </Button>
             </div>
           </div>
