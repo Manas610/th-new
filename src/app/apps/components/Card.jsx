78e504fc7c1ef6f1a5a3ecda64c9331902405698
Manas <singhmanas610@gmail.com>
2024-04-09 17:43:21 +0530

    app-portfolio card image fix

--- src/app/apps/components/Card.jsx
+++ src/app/apps/components/Card.jsx
@@ -5,14 +5,14 @@ function Card({ title, description , image }) {
   const url = `/apps/${encodeURIComponent(title)}`;
 
     return (
-      <div className="bg-white hover:shadow-xl hover:shadow-slate-200 rounded-lg overflow-hidden h-72">
+      <div className="bg-white hover:shadow-xl hover:shadow-slate-200 rounded-lg overflow-hidden sm:h-72">
         <div className="grid sm:grid-cols-5 gap-4">
-          <div className="md:col-span-2 flex flex-col justify-center items-center rounded-lg h-72">
+          <div className="sm:col-span-2 flex flex-col justify-center items-center rounded-lg h-72">
             <div className="p-8 rounded-lg bg-sky-100">
-              <img src={image} alt="App Logo" className="w-40 h-40 object-cover" />
+              <img src={image} alt="App Logo" className="w-40 h-40 object-contain" />
             </div>
           </div>
-          <div className="md:col-span-3 p-4 flex flex-col justify-between">
+          <div className="sm:col-span-3 p-4 flex flex-col justify-between">
             <div>
               <h2 className="text-2xl font-bold">{title}</h2>
               <p className="mt-2 text-gray-600">{description}</p>
