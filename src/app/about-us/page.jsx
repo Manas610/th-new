6c825674f7d5b2001654c7bdea95f892fab1da13
Manas <singhmanas610@gmail.com>
2024-04-23 23:03:44 +0530

    about us page structure

---
+++ src/app/about-us/page.jsx
@@ -0,0 +1,31 @@
+"use client"
+
+function page() {
+  return (
+    <div className="min-h-screen">
+        <div className="my-32">
+            <div className="bg-blue-dark h-72 flex flex-col justify-center items-center">
+                <span className="text-white text-7xl font-semibold">About Us</span>
+            </div>
+        </div>
+
+        <div className="lg:mx-16 md:mx-8 mx-4 md:grid md:grid-cols-2 flex flex-col">
+            <div className="lg:m-16 md:m-8 m-4 mb-8">
+                <span className="text-4xl font-bold">The Best Solutions for Best Business</span>
+            </div>
+            <div className="text-slate-600 sm:grid sm:grid-cols-2 flex flex-col gap-8 mb-8 mx-4">
+                <div className="col-span-1 text-justify">
+                    <span>We are a team of experienced professionals who specialize in web development, app development, logo and branding, SEO optimization, digital marketing, and strategy and research.</span><br/><br/>
+                    <span>Our company was founded on the principle of delivering high-quality work and exceptional customer service. We take pride in the results we achieve and we are committed to staying up-to-date with the latest industry trends and technologies.</span><br/>
+                </div>
+                <div className="col-span-1 text-justify">
+                    <span>We understand that every client is unique and we work closely with them to understand their goals and create solutions that will help them achieve success online.</span><br/><br/>
+                    <span>Whether you're a small startup or a large corporation, we have the skills and expertise to help take your online presence to the next level. We are dedicated to providing a personalized and responsive service to ensure our clients complete satisfaction.</span><br/>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default page
--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,61 +1,78 @@
+"use client"
+
+import { Button, Checkbox, Label, TextInput, Textarea } from "flowbite-react";
+
 function page() {
   return (
     <div className="min-h-screen">
       
       <div className="mt-32">
-        <div className="bg-orange-400 h-72 flex flex-col justify-center items-center">
+        <div className="bg-blue-dark h-72 flex flex-col justify-center items-center">
             <span className="text-white text-7xl font-semibold">Contact Us</span>
         </div>
       </div>
 
     <div className="flex justify-center items-center bg-gray-100 my-16">
-      <form className="bg-white p-8 shadow-lg rounded-3xl">
-        <div className="space-y-6">
-          <div>
-            <label htmlFor="name" className="text-sm font-medium text-gray-700 block mb-2">Name</label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              className="w-full p-2 border border-gray-300 rounded-md"
-              required
-            />
+      <form className="flex max-w-md flex-col gap-4 rounded-3xl border-2 p-8">
+        <span className="font-bold">Plaese Fill the Form</span>
+        <hr />
+        <div>
+          <div className="mb-2 block">
+            <Label htmlFor="name" value="Name" />
           </div>
-          <div>
-            <label htmlFor="email" className="text-sm font-medium text-gray-700 block mb-2">Email</label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              className="w-full p-2 border border-gray-300 rounded-md"
-              required
-            />
-          </div>
-          <fieldset className="space-y-4">
-            <legend className="text-sm font-medium text-gray-700 mb-2">Services</legend>
-            <div className="md:flex md:flex-row md:space-x-4 grid sm:grid-cols-2 grid-cols-1">
-              <label className="inline-flex items-center space-x-2">
-                <input type="checkbox" name="services" value="app" />
-                <span>Application Development</span>
-              </label>
-              <label className="inline-flex items-center space-x-2">
-                <input type="checkbox" name="services" value="web" />
-                <span>Website Development</span>
-              </label>
-              <label className="inline-flex items-center space-x-2">
-                <input type="checkbox" name="services" value="logo" />
-                <span>Logo</span>
-              </label>
-              <label className="inline-flex items-center space-x-2">
-                <input type="checkbox" name="services" value="seo" />
-                <span>SEO(Search Engine Optimization)</span>
-              </label>
-            </div>
-          </fieldset>
-          <button type="submit" className="w-full p-3 bg-blue-dark text-white rounded-md hover:bg-blue-800 transition-colors">
-            Submit
-          </button>
+          <TextInput id="name" type="text" required shadow />
         </div>
+        <div>
+          <div className="mb-2 block">
+            <Label htmlFor="email" value="Your email" />
+          </div>
+          <TextInput id="email" type="email" placeholder="name@email.com" required shadow />
+        </div>
+        <div>
+          <div className="mb-2 block">
+            <Label htmlFor="mobile" value="Mobile Number" />
+          </div>
+          <TextInput id="mobile" type="number" required shadow />
+        </div>
+        <div>
+          <div className="mb-2 block">
+            <Label htmlFor="company" value="Company" />
+          </div>
+          <TextInput id="company" type="text" shadow />
+        </div>
+        <div>
+          <div className="mb-2 block">
+            <Label htmlFor="description" value="description" />
+          </div>
+          <Textarea id="description" type="text" shadow />
+        </div>
+        <div className="grid grid-cols-2 gap-4">
+          <div className="flex items-center gap-2">
+            <Checkbox id="web" />
+            <Label htmlFor="web">
+              Website Development
+            </Label>
+          </div>
+          <div className="flex items-center gap-2">
+            <Checkbox id="app" />
+            <Label htmlFor="app">
+              App Development
+            </Label>
+          </div>
+          <div className="flex items-center gap-2">
+            <Checkbox id="logo" />
+            <Label htmlFor="logo">
+              Logo Designing
+            </Label>
+          </div>
+          <div className="flex items-center gap-2">
+            <Checkbox id="seo" />
+            <Label htmlFor="seo">
+              SEO
+            </Label>
+          </div>
+        </div>
+        <Button type="submit" className="bg-blue-dark">Request A Service</Button>
       </form>
     </div>
 
