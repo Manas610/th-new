7fd02cbb02e3255224d6da7dae44593e5b9c5a50
Manas <singhmanas610@gmail.com>
2024-05-08 09:54:26 +0530

    changed glassmorphism tint to blue in logo portfolio

--- src/app/logos/components/Card.jsx
+++ src/app/logos/components/Card.jsx
@@ -4,8 +4,8 @@ function Card({ title , logo , date }) {
     return (
       <div>
         <div className="flex flex-col bg-white rounded-3xl overflow-hidden shadow-lg hover:bg-opacity-80 group"> 
-          <div className="flex flex-col h-56 items-center justify-center bg-sky-100 mx-4 mt-4 p-8 rounded-2xl relative">
-            <Image className="group-hover:drop-shadow-xl ease-linear duration-200" src={logo} height={180} width={180} />
+          <div className="flex flex-col h-56 items-center justify-center bg-sky-100 mx-4 mt-4 px-8 pt-4 pb-8 rounded-2xl relative">
+            <Image className="group-hover:drop-shadow-2xl ease-linear duration-200" src={logo} height={180} width={180} />
             <div className="text-xs font-semibold absolute bottom-3 right-3 p-2 rounded-xl glassmorphism-xl shadow-md">{date}</div>
           </div>
           <div className="flex flex-col py-4 font-semibold text-lg text-center">{title}</div>
